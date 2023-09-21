c27640a2c8296984779d9db3914a0c715d4ffc1c
hwkim <kjs764644@gmail.com>
2023-09-21 09:49:40 +0900

    project

---
+++ 1.배스킨라빈스31게임/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0)); 
+
+    int number = 0;
+    bool turn = true; 
+
+    while (number < 31) {
+        cout << "현재 숫자: " << number << endl;
+        if (turn) {
+            cout << "플레이어 차례입니다.(1~3까지 입력하세요)" << endl;
+
+            int input;
+            cin >> input;
+            while (input > 3) {
+                cout << "범위에서 벗어났습니다.  다시입력해주세요" << endl;
+                cin >> input;
+            }
+            number += input;
+        }
+        else {
+            cout << "컴퓨터 차례입니다." << endl;
+
+            int input = rand() % 3 + 1; 
+            cout << "컴퓨터가 선택한 숫자: " << input << endl;
+
+            number += input;
+        }
+
+        turn = !turn; 
+    }
+
+    if (turn) {
+        cout << "플레이어가 이겼습니다!" << endl;
+    }
+    else {
+        cout << "컴퓨터가 이겼습니다!" << endl;
+    }
+
+    return 0;
+}
+
---
+++ 7.게임캐릭터만들기/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+
+class Weapon {
+public:
+    std::string name;
+    int damage;
+    int attackCount;
+
+    Weapon(std::string name, int damage, int attackCount) : name(name), damage(damage), attackCount(attackCount) {}
+
+    virtual void attack() = 0; 
+};
+
+
+class Knife : public Weapon {
+public:
+    Knife() : Weapon("칼", 5, 3) {}
+
+    void attack() override {
+        std::cout << "찌르기" << std::endl;
+    }
+};
+
+
+class Gun : public Weapon {
+public:
+    Gun() : Weapon("총", 10, 1) {}
+
+    void attack() override {
+        std::cout << "총쏘기" << std::endl;
+    }
+};
+
+
+class Character {
+public:
+    int hp;
+    int level;
+    std::vector<Weapon*> weapons;
+
+    Character(int hp, int level) : hp(hp), level(level) {}
+
+    
+    void pickUpWeapon(Weapon* weapon) {
+        weapons.push_back(weapon);
+        std::cout << weapon->name << "을(를) 주웠습니다." << std::endl;
+    }
+
+    
+    void attack(Character& target, Weapon* weapon) {
+        if (weapon->attackCount > 0) {
+            target.hp -= weapon->damage;
+            weapon->attackCount--;
+            weapon->attack();
+            std::cout << weapon->name << "으로 공격하여 상대의 HP가 " << target.hp << "가 되었습니다." << std::endl;
+        }
+        else {
+            std::cout << weapon->name << "은(는) 더 이상 사용할 수 없습니다." << std::endl;
+        }
+    }
+};
+
+int main() {
+    Character character1(100, 1);
+    Character character2(100, 1);
+
+    Knife knife;
+    Gun gun;
+
+    character1.pickUpWeapon(&knife);
+    character2.pickUpWeapon(&gun);
+
+    
+    character1.attack(character2, &knife);
+    character2.attack(character1, &gun);
+
+    return 0;
+}
